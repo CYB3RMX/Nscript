4338953d6096970ef2878a2bcc9f520c41d8019e
CYB3RMX <42123683+CYB3RMX@users.noreply.github.com>
2018-11-07 03:16:51 +0300

    Add files via upload

--- nscript.sh
+++ nscript.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #-*- encoding: utf-8 -*-
 
-#------------COLOR AND FONT AREA-------------@
+#------------COLOR ANT FONT AREA-------------@
 lg1='\e[92m'
 lg2='\e[102m'
 lr1='\e[91m'
@@ -12,6 +12,7 @@ black1='\e[30m'
 lc1='\e[96m'
 ly1='\e[93m'
 ly2='\e[103m'
+lm1='\e[95m'
 w1='\e[0m'
 defbl='\e[49m'
 #------------GRAPHICS AREA-------------@
@@ -21,8 +22,8 @@ quit()
   echo -en $lg1"\a\n\n["$lr1"+"$lg1"]"$w1" QUIT ? [Y/N]\n"
   read quitz
   case $quitz in
-  Y) echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" GOODBYE..\n" ;;
-  y) echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" GOODBYE..\n" ;;
+  Y) echo -en $lg1"\a\n["$lr1"+"$lg1"]"$w1" GOODBYE..\n" ;;
+  y) echo -en $lg1"\a\n["$lr1"+"$lg1"]"$w1" GOODBYE..\n" ;;
   N) clear
      cd
      ./nscript.sh ;;
@@ -80,7 +81,10 @@ opening()
  echo -en $lg1"["$lr1"1"$lg1"]"$w1" PORT ATTACKS\n"
  echo -en $lg1"["$lr1"2"$lg1"]"$w1" ROUTER ATTACKS\n"
  echo -en $lg1"["$lr1"3"$lg1"]"$w1" SQL DATA ATTACK\n"
- echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:\n"
+ echo -en $lg1"["$lr1"4"$lg1"]"$w1" IOS HACKING\n"
+ echo -en $lg1"["$lr1"5"$lg1"]"$w1" BRUTE FORCE\n"
+ echo -en $lg1"["$lr1"x"$lg1"]"$w1" QUIT...\n"
+ echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:"
  read nscripts
 }
 opening
@@ -104,7 +108,7 @@ ports()
   echo -en $lg1"["$lr1"7"$lg1"]"$w1" SAMBA HACKING\n"
   echo -en $lg1"["$lr1"8"$lg1"]"$w1" RTSP HACKING\n"
   echo -en $lg1"["$lr1"x"$lg1"]"$w1" GO BACK >>\n"
-  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:\n"
+  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:"
   read portz
 }
 #------------ROUTER ATTACK OPTION-------------@
@@ -122,6 +126,7 @@ routers()
   echo -en $lg1"["$lr1"2"$lg1"]"$w1" DLINK\n"
   echo -en $lg1"["$lr1"3"$lg1"]"$w1" TPLINK\n"
   echo -en $lg1"["$lr1"x"$lg1"]"$w1" GO BACK >>\n"
+  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:"
   read routerz
 }
 #------------SQL DATA ATTACK OPTION-------------@
@@ -143,8 +148,58 @@ sql()
   echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" TARGET => "$lc1$target
   echo -en $lg1"\n\n["$lr1"1"$lg1"]"$w1" AVAILABLE SCRIPTS\n"
   echo -en $lg1"["$lr1"x"$lg1"]"$w1" GO BACK >>\n"
-  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:\n"
+  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:"
   read sql_select
+}
+ios_hack()
+{
+  clear
+  echo -en $lg1"  _____ ____   _____\a\n"
+  echo -en " |_   _/ __ \ / ____| \n"
+  echo -en "   | || |  | | (___\n"
+  echo -en "   | || |  | |\___ \ \n"
+  echo -en "  _| || |__| |____) | \n"
+  echo -en " |_____\____/|_____/ \n"
+  echo -en $ly1"       _    _          _____ _  __\n"
+  echo -en "      | |  | |   /\   / ____| |/ / \n"
+  echo -en "      | |__| |  /  \ | |    |   / \n"
+  echo -en "      |  __  | / /\ \| |    |  < \n"
+  echo -en "      | |  | |/ ____ \ |____|   \ \n"
+  echo -en "      |_|  |_/_/    \_\_____|_|\_\ \n"
+  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" TARGET => "$lc1$target
+  echo -en $lg1"\n\n["$lr1"1"$lg1"]"$w1" AVAILABLE SCRIPTS\n"
+  echo -en $lg1"["$lr1"x"$lg1"]"$w1" GO BACK >>\n"
+  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:"
+  read ios
+}
+brute_force()
+{
+  clear
+  echo -en $lm1"  ____  _____  _    _ _______ ______\a\n"
+  echo -en " |  _ \|  __ \| |  | |__   __|  ____| \n"
+  echo -en " | |_) | |__) | |  | |  | |  | |__\n"
+  echo -en " |  _ <|  _  /| |  | |  | |  |  __| \n"
+  echo -en " | |_) | | \ \| |__| |  | |  | |____\n"
+  echo -en " |____/|_|  \_ \____/   |_|  |______| \n"
+  echo -en $lr1"       ______ ____  _____   _____ ______\n"
+  echo -en "      |  ____/ __ \|  __ \ / ____|  ____| \n"
+  echo -en "      | |__ | |  | | |__) | |    | |__\n"
+  echo -en "      |  __|| |  | |  _  /| |    |  __| \n"
+  echo -en "      | |   | |__| | | \ \| |____| |____\n"
+  echo -en "      |_|    \____/|_|  \_\______|______| \n"
+  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" TARGET => "$lc1$target
+  echo -en $lg1"\n\n["$lr1"1"$lg1"]"$w1" FTP BRUTE\n"
+  echo -en $lg1"["$lr1"2"$lg1"]"$w1" SSH BRUTE\n"
+  echo -en $lg1"["$lr1"3"$lg1"]"$w1" TELNET BRUTE\n"
+  echo -en $lg1"["$lr1"4"$lg1"]"$w1" SMTP BRUTE\n"
+  echo -en $lg1"["$lr1"5"$lg1"]"$w1" SNMP BRUTE\n"
+  echo -en $lg1"["$lr1"6"$lg1"]"$w1" HTTP BRUTE\n"
+  echo -en $lg1"["$lr1"7"$lg1"]"$w1" DNS BRUTE\n"
+  echo -en $lg1"["$lr1"8"$lg1"]"$w1" MYSQL BRUTE\n"
+  echo -en $lg1"["$lr1"9"$lg1"]"$w1" VNC BRUTE\n"
+  echo -en $lg1"["$lr1"x"$lg1"]"$w1" GO BACK >>\n"
+  echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" CHOOSE:"
+  read force
 }
 case $nscripts in
 1) ports
@@ -261,4 +316,88 @@ case $nscripts in
       clear
       ./nscript.sh ;;
    esac ;;
+4) ios_hack
+   case $ios in
+   1) cd ..;cd usr/share/nmap/scripts
+      ls | grep ios
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   x) cd
+      clear
+      ./nscript.sh ;;
+   esac ;;
+5) brute_force
+   case $force in
+   1) cd ..;cd usr/share/nmap/scripts
+      ls | grep ftp-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   2) cd ..;cd usr/share/nmap/scripts
+      ls | grep ssh-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   3) cd ..;cd usr/share/nmap/scripts
+      ls | grep telnet-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   4) cd ..;cd usr/share/nmap/scripts
+      ls | grep smtp-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   5) cd ..;cd usr/share/nmap/scripts
+      ls | grep snmp-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   6) cd ..;cd usr/share/nmap/scripts
+      ls | grep http-brute
+      ls | grep http-auth
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   7) cd ..;cd usr/share/nmap/scripts
+      ls | grep dns-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   8) cd ..;cd usr/share/nmap/scripts
+      ls | grep mysql-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   9) cd ..;cd usr/share/nmap/scripts
+      ls | grep vnc-brute
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" YOUR SCRIPT:\n"
+      read scriptx
+      echo -en $lg1"\n["$lr1"+"$lg1"]"$w1" ATTACK STARTED..\n"
+      nmap -Pn -sV --script $scriptx $target
+      quit ;;
+   x) cd
+      clear
+      ./nscript.sh ;;
+   esac ;;
+x) quit ;;
 esac
